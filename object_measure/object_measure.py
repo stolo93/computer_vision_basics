21fe11ffd765af2223ebc1a038a0e75b07c5fdc6
Samuel <xstola03@stud.fit.vutbr.cz>
2023-09-21 22:22:59 +0200

    Tinkering with the parameters

--- object_measure/object_measure.py
+++ object_measure/object_measure.py
@@ -21,7 +21,7 @@ def main():
 
     args = parser.parse_args()
     try:
-        referal_width = float(args.width)
+        referral_width = float(args.width)
     except ValueError as e:
         print(e)
         exit(1)
@@ -29,7 +29,7 @@ def main():
     image = cv2.imread(args.image)
     assert image is not None, f'Invalid image path {args.image}'
 
-    desired_max_width = 1200
+    desired_max_width = 640
 
     # Resize the image if needed
     if image.shape[1] > desired_max_width:
@@ -41,16 +41,19 @@ def main():
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.Canny(gray, 100, 200)
     edged = cv2.dilate(edged, None, iterations=1)
     edged = cv2.erode(edged, None, iterations=1)
 
+    cv2.imshow('edged picture', edged)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    cnts, _ = contours.sort_contours(cnts)
+    cnts, _ = contours.sort_contours(cnts)  # Sort contours from left to right
 
     pixels_per_metric = None
-    orig = image.copy()
     for c in cnts:
         # If contour area is not big enough ingore it
         if cv2.contourArea(c) < 100:
@@ -64,17 +67,16 @@ def main():
         # Order the points such that they appear to be:
         # top-left, top-right, bottom-left, bottom-right
         # then draw the outline of the rotated bounding box
-
         box = perspective.order_points(box)
-        cv2.drawContours(orig, [box.astype('int')], -1, (0, 255, 0), 2)
+        cv2.drawContours(image, [box.astype('int')], -1, (0, 255, 0), 2)
 
         for x, y in box:
-            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(image, (int(x), int(y)), 5, (0, 0, 255), cv2.FILLED)
 
         # Unpack box and compute the midpoint between
         # top left <-> top right
         # bottom left <-> bottom right
-        # and so on
+        # and so on...
         tl, tr, br, bl = box
         tltr_x, tltr_y = midpoint(tl, tr)
         blbr_x, blbr_y = midpoint(bl, br)
@@ -82,13 +84,13 @@ def main():
         tlbl_x, tlbl_y = midpoint(tl, bl)
         trbr_x, trbr_y = midpoint(tr, br)
 
-        cv2.circle(orig, (tltr_x, tltr_y), 5, (255, 0, 0), cv2.FILLED)
-        cv2.circle(orig, (blbr_x, blbr_y), 5, (255, 0, 0), cv2.FILLED)
-        cv2.circle(orig, (tlbl_x, tlbl_y), 5, (255, 0, 0), cv2.FILLED)
-        cv2.circle(orig, (trbr_x, trbr_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(image, (tltr_x, tltr_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(image, (blbr_x, blbr_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(image, (tlbl_x, tlbl_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(image, (trbr_x, trbr_y), 5, (255, 0, 0), cv2.FILLED)
 
-        cv2.line(orig, (tltr_x, tltr_y), (blbr_x, blbr_y), (255, 0, 255), 1)
-        cv2.line(orig, (tlbl_x, tlbl_y), (trbr_x, trbr_y), (255, 0, 255), 1)
+        cv2.line(image, (tltr_x, tltr_y), (blbr_x, blbr_y), (255, 0, 255), 1)
+        cv2.line(image, (tlbl_x, tlbl_y), (trbr_x, trbr_y), (255, 0, 255), 1)
 
         # Compute size in pixels
         distance_a = dist((tlbl_x, tlbl_y), (trbr_x, trbr_y))
@@ -96,15 +98,15 @@ def main():
 
         # Compute pixels per metric ration based on the dimension provided
         if pixels_per_metric is None:
-            pixels_per_metric = referal_width / distance_a
+            pixels_per_metric = referral_width / distance_a
 
         width = distance_a * pixels_per_metric
         height = distance_b * pixels_per_metric
 
-        cv2.putText(orig, f'{height:.2f}', (tltr_x - 10, tltr_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1)
-        cv2.putText(orig, f'{width:.2f}', (tlbl_x + 10, tlbl_y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1)
+        cv2.putText(image, f'{height:.2f}', (tltr_x - 10, tltr_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1)
+        cv2.putText(image, f'{width:.2f}', (tlbl_x + 10, tlbl_y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1)
 
-    cv2.imshow('Frame', orig)
+    cv2.imshow('Frame', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
