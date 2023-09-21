59a8efe40d63bb09ecca519d33d1f075411d8938
Samuel <xstola03@stud.fit.vutbr.cz>
2023-09-21 20:47:01 +0200

    Added object recognition:

--- object_measure/object_measure.py
+++ object_measure/object_measure.py
@@ -1,4 +1,12 @@
 import argparse
+import cv2
+import imutils
+import numpy as np
+from imutils import contours
+from imutils import perspective
+
+def midpoint(pt_a: tuple, pt_b: tuple):
+    return int((pt_a[0] + pt_b[0]) * 0.5), int((pt_a[1] + pt_b[1]) * 0.5)
 
 
 def main():
@@ -10,6 +18,72 @@ def main():
     parser.add_argument('-s', '--size', required=True)  # Size of the reference 'leftmost' object
 
     args = parser.parse_args()
+    image = cv2.imread(args.image)
+    desired_max_width = 500
+
+    # Resize the image if needed
+    if image.shape[1] > desired_max_width:
+        scale_factor = float(desired_max_width) / float(image.shape[1])
+        dim = (int(image.shape[0] * scale_factor), int(image.shape[1] * scale_factor))
+        image = cv2.resize(image, dim)
+
+    # Convert to grayscale image before edge detection
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=1)
+    edged = cv2.erode(edged, None, iterations=1)
+
+    cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts, _ = contours.sort_contours(cnts)
+
+    pixels_per_metric = None
+    orig = image.copy()
+    for c in cnts:
+        # If contour area is not big enough ingore it
+        if cv2.contourArea(c) < 100:
+            continue
+
+        # Rotated bounding rect
+        box = cv2.minAreaRect(c)
+        box = cv2.boxPoints(box)
+        box = np.array(box, dtype='int')
+
+        # Order the points such that they appear to be:
+        # top-left, top-right, bottom-left, bottom-right
+        # then draw the outline of the rotated bounding box
+
+        box = perspective.order_points(box)
+        cv2.drawContours(orig, [box.astype('int')], -1, (0, 255, 0), 2)
+
+        for x, y in box:
+            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), cv2.FILLED)
+
+        # Unpack box and compute the midpoint between
+        # top left <-> top right
+        # bottom left <-> bottom right
+        # and so on
+        tl, tr, br, bl = box
+        tltr_x, tltr_y = midpoint(tl, tr)
+        blbr_x, blbr_y = midpoint(bl, br)
+
+        tlbl_x, tlbl_y = midpoint(tl, bl)
+        trbr_x, trbr_y = midpoint(tr, br)
+
+        cv2.circle(orig, (tltr_x, tltr_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(orig, (blbr_x, blbr_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(orig, (tlbl_x, tlbl_y), 5, (255, 0, 0), cv2.FILLED)
+        cv2.circle(orig, (trbr_x, trbr_y), 5, (255, 0, 0), cv2.FILLED)
+
+        cv2.line(orig, (tltr_x, tltr_y), (blbr_x, blbr_y), (255, 0, 255), 1)
+        cv2.line(orig, (tlbl_x, tlbl_y), (trbr_x, trbr_y), (255, 0, 255), 1)
+
+
+    cv2.imshow('Frame', orig)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
